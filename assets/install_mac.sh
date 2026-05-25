1cfa7626b20997ae25441cfac166ed1b38ab18fe
Yloan Doumeix--Ballester <yloan@Air-de-Yloan.lan>
2026-05-25 22:04:00 +0200

    really just the aegon stuff rn

--- assets/install_mac.sh
+++ assets/install_mac.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "Installation de BLITZKRIEG..."
 
-curl -L https://github.com/Yloan/Blitzkrieg/archive/refs/heads/main.zip -o /tmp/blitzkrieg.zip
-
+curl -L https://github.com/Yloan/Blietzkrieg_Game_final/archive/refs/heads/main.zip -o /tmp/blitzkrieg.zip
 unzip -o /tmp/blitzkrieg.zip -d ~/Library/Application\ Support/
-rm /tmp/blitzkrieg.zip
 
-cd ~/Library/Application\ Support/Blitzkrieg-main
+rm /tmp/blitzkrieg.zip
+cd ~/Library/Application\ Support/Blietzkrieg_Game_final-main
 
 python3 -m venv .venv
 .venv/bin/pip install colorama==0.4.6 pygame==2.6.1 -q
--- assets/install_windows.bat
+++ assets/install_windows.bat
@@ -1,12 +1,14 @@
 @echo off
 echo Installation de BLITZKRIEG...
 
-curl -L https://github.com/Yloan/Blitzkrieg/archive/refs/heads/main.zip -o %TEMP%\blitzkrieg.zip
+curl -L https://github.com/Yloan/Blietzkrieg_Game_final/archive/refs/heads/main.zip -o %TEMP%\blitzkrieg.zip
 tar -xf %TEMP%\blitzkrieg.zip -C %LOCALAPPDATA%
-del %TEMP%\blitzkrieg.zip
 
-cd %LOCALAPPDATA%\Blitzkrieg-main
-pip install colorama==0.4.6 pygame==2.6.1
+del %TEMP%\blitzkrieg.zip
+cd %LOCALAPPDATA%\Blietzkrieg_Game_final-main
+
+python -m venv .venv
+.venv\Scripts\pip install colorama==0.4.6 pygame==2.6.1
 
 echo Lancement du jeu...
-python src\main.py
+.venv\Scripts\python src\main.py
